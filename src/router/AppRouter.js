b1cdafc1c636d205fd65802c249874eab54fd2e3
ArwaYearub <Arwa.Yaareb@gmail.com>
2024-12-02 15:13:00 +0300

    finishing with displaying, delete and edit buttons work perfectly

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -4,6 +4,8 @@ import Header from "../components/Header";
 import AddBook from "../components/AddBook";
 import BooksList from "../components/BooksList";
 import useLocalStorage from "../hooks/useLocalStorage";
+import EditBook from "../components/EditBook";
+import { Redirect } from "react-router";
 
 const AppRouter = () => {
   const [books, setBooks] = useLocalStorage("books", []);
@@ -14,13 +16,26 @@ const AppRouter = () => {
         <Header />
         <div className="main-content">
           <Switch>
-            <Route component={BooksList} path="/" exact={true} />
+            <Route
+              render={(props) => (
+                <BooksList {...props} books={books} setBooks={setBooks} />
+              )}
+              path="/"
+              exact={true}
+            />
             <Route
               render={(props) => (
                 <AddBook {...props} books={books} setBooks={setBooks} />
               )}
               path="/add"
             />
+            <Route
+              render={(props) => (
+                <EditBook {...props} books={books} setBooks={setBooks} />
+              )}
+              path="/edit/:id"
+            />
+            <Route component={() => <Redirect to="/" />} />
           </Switch>
         </div>
       </div>
